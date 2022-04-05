5b99e817e8d1e88ed0fd25dbb8f490c00d50c6ac
wesley <wesbourkeavl@gmail.com>
2022-04-05 14:38:24 -0400

    commit tree 0e6f0070c20f3b677be76ece385bf98bb1fdd2e9

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre 
 
+    def initialize(title)
+        @title = title
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end    
+    
+end     
+
+new_book = Book.new "And Then There Were None"
+new_book.author = "Agatha Christie"
 
